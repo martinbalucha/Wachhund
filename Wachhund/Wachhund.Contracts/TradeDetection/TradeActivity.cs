491b559698923216f5ee8cb7ebc0aabb84f3d58d
martinbalucha <martin.balucha@outlook.com>
2024-08-10 19:01:51 +0200

    Added source id
    Added rough draft of the trade activity interfaces and domain objects

---
+++ Wachhund/Wachhund.Contracts/TradeDetection/TradeActivity.cs
@@ -0,0 +1,8 @@
+﻿namespace Wachhund.Contracts.TradeDetection;
+
+public enum TradeActivity
+{
+    None = 0,
+    Buy = 1,
+    Sell = 2
+}
---
+++ Wachhund/Wachhund.Contracts/TradeDetection/TradeDeal.cs
@@ -0,0 +1,11 @@
+﻿namespace Wachhund.Contracts.TradeDetection;
+
+/// <summary>
+/// Represents performed trade deal.
+/// </summary>
+/// <param name="Id"></param>
+/// <param name="Balance"></param>
+/// <param name="CurrencyPair"></param>
+/// <param name="Lot"></param>
+/// <param name="Activity"></param>
+public record TradeDeal(Guid Id, Guid SourceId, decimal Balance, string CurrencyPair, decimal Lot, TradeActivity Activity);
