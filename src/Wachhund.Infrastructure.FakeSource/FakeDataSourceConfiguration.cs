c9f035ec49b7b164d12f6d0daf984c2d585795b7
martinbalucha <martin.balucha@outlook.com>
2024-08-10 21:39:34 +0200

    Added Bogus and configuration

---
+++ src/Wachhund.Infrastructure.FakeSource/FakeDataSourceConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wachhund.Infrastructure.FakeSource;
+
+public record FakeDataSourceConfiguration
+{
+    public string[] AllowedCurrenciesIso4217 { get; init; } = [];
+}
