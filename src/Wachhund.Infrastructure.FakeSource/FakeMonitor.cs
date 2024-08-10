f8232615a6b1e9affa63479d96128165e5f6482e
martinbalucha <martin.balucha@outlook.com>
2024-08-10 21:00:42 +0200

    Added classes draft

---
+++ src/Wachhund.Infrastructure.FakeSource/FakeMonitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wachhund.Contracts.TradeDetection;
+
+namespace Wachhund.Infrastructure.FakeSource;
+
+public class FakeMonitor : IMonitor
+{
+    public Task StartAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Wachhund.Infrastructure.FakeSource/IFakeDataSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wachhund.Infrastructure.FakeSource;
+
+/// <summary>
+/// Represents fake source for trade deals.
+/// </summary>
+public interface IFakeDataSource
+{
+}
