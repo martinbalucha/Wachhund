f91fa5960b45fcdbe2e5ec3b1b695c3979dd7785
martinbalucha <martin.balucha@outlook.com>
2024-08-11 20:55:19 +0200

    Added in memory cache barebones

---
+++ src/Wachhund.Domain/InMemoryTradeDealCache.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wachhund.Contracts.TradeDetection;
+using Wachhund.Contracts.TradeDetection.Persistence;
+
+namespace Wachhund.Domain;
+
+public class InMemoryTradeDealCache : ITradeDealCache
+{
+    private readonly ConcurrentDictionary<string, ConcurrentBag<TradeDeal>> _cache = new();
+
+    public Task CleanupCacheAsync(DateTimeOffset cutoffDate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<TradeDeal>> GetDealsLaterThenAsync(string currencyPair, DateTimeOffset latestDealsDate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StoreAsync(TradeDeal tradeDeal)
+    {
+        throw new NotImplementedException();
+    }
+}
