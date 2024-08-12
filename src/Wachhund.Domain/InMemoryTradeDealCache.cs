6bc6c85423088b1581bf5364455f83de8a6167d1
martinbalucha <martin.balucha@outlook.com>
2024-08-12 20:35:54 +0200

    Finished cache draft

--- src/Wachhund.Domain/InMemoryTradeDealCache.cs
+++ src/Wachhund.Domain/InMemoryTradeDealCache.cs
@@ -1,9 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Wachhund.Contracts.TradeDetection;
 using Wachhund.Contracts.TradeDetection.Persistence;
 
@@ -11,26 +7,52 @@ namespace Wachhund.Domain;
 
 public class InMemoryTradeDealCache : ITradeDealCache
 {
+    private readonly ILogger<InMemoryTradeDealCache> _logger;
+
     /// <summary>
     /// An in-memory cache which separates the deals into "buckets" by the currency pair.
     /// </summary>
-    private readonly ConcurrentDictionary<string, ConcurrentBag<TradeDeal>> _cache = new();
+    private readonly ConcurrentDictionary<string, ConcurrentDictionary<TradeDeal, byte>> _cache = new();
+
+    public InMemoryTradeDealCache(ILogger<InMemoryTradeDealCache> logger)
+    {
+        _logger = logger;
+    }
 
     public Task CleanupCacheAsync(DateTimeOffset cutoffDate)
     {
-        throw new NotImplementedException();
+        Parallel.ForEach(_cache, bucket =>
+        {
+            var oldDeals = bucket.Value.Where(kv => kv.Key.OccurredAt > cutoffDate);
+            
+            foreach (var oldDeal in oldDeals)
+            {
+                if (!bucket.Value.TryRemove(oldDeal))
+                {
+                    _logger.LogDebug("Could not remove deal with ID='{DealId}'", oldDeal.Key.Id);
+                }
+            }
+        });
+
+        return Task.CompletedTask;
     }
 
     public Task<IEnumerable<TradeDeal>> GetDealsLaterThenAsync(string currencyPair, DateTimeOffset latestDealsDate)
     {
-        throw new NotImplementedException();
+        if (!_cache.TryGetValue(currencyPair, out var soughtDeals))
+        {
+            return Task.FromResult(Enumerable.Empty<TradeDeal>());
+        }
+
+        return Task.FromResult(soughtDeals.Select(kv => kv.Key)
+                                          .Where(d => d.OccurredAt <= latestDealsDate));
     }
 
     public Task StoreAsync(TradeDeal tradeDeal)
     {
         var dealsForCurrency = _cache.GetOrAdd(tradeDeal.CurrencyPair, (k) => new ());
 
-        dealsForCurrency.Add(tradeDeal);
+        dealsForCurrency.TryAdd(tradeDeal, default);
 
         return Task.CompletedTask;
     }
